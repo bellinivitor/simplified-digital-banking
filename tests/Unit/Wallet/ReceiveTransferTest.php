b382eb788435fa4636df659e4a0e4f6071fa5392
bellinivitor <bellinivitor@gmail.com>
2023-09-12 21:50:22 -0300

    Created tests to ReceiveTransferAction;

---
+++ tests/Unit/Wallet/ReceiveTransferTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Natural;
+use App\Models\User;
+use App\Models\Wallet;
+use Domain\Wallet\Actions\DescountValueOfTransferAction;
+use Domain\Wallet\Actions\ReceiveTransferAction;
+use function PHPUnit\Framework\assertEquals;
+
+test('Validate value received on wallet of recipient', function () {
+
+    /** @var User $user */
+    $user = User::factory()
+        ->has(Natural::factory()
+            ->has(Wallet::factory()->withBalance(500))
+        )
+        ->create();
+
+    $wallet = $user->natural->wallet;
+
+    $receiveTransferAction = resolve(ReceiveTransferAction::class);
+    $receiveTransferAction($wallet, 100);
+
+    assertEquals(600, $wallet->balance);
+
+})->group('Unit', 'Wallet');
